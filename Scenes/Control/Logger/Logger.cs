1d351ae49101460703e7f0a7f68edbc70bc3448a
Zibeline <zibeline.dev@gmail.com>
2025-07-16 19:47:17 +0200

    💄 Logger auto-scroll
    + new icon

--- Scenes/Control/Logger/Logger.cs
+++ Scenes/Control/Logger/Logger.cs
@@ -5,12 +5,27 @@ public partial class Logger : PanelContainer
         public static Logger Instance { get; private set; }
 
         private VBoxContainer container;
+        private ScrollContainer scrollContainer;
+
+        private ScrollBar scrollBar;
+        private bool scrollRequested;
 
         public override void _Ready()
         {
                 Instance = this;
 
                 container = GetNode<VBoxContainer>("%VBoxContainer");
+                scrollContainer = GetNode<ScrollContainer>("%ScrollContainer");
+
+                scrollBar = scrollContainer.GetVScrollBar();
+        }
+
+        public override void _Process(double delta)
+        {
+                if (scrollRequested)
+                {
+                        UpdateScrollValue();
+                }
         }
 
         public static void Log(string message)
@@ -20,5 +35,14 @@ public partial class Logger : PanelContainer
                 label.Text = message;
 
                 Instance.container.AddChild(label);
+                Instance.scrollRequested = true;
+                Instance.SetProcess(true);
+        }
+
+        public void UpdateScrollValue()
+        {
+                scrollContainer.ScrollVertical = (int)scrollBar.MaxValue;
+                scrollRequested = false;
+                SetProcess(false);
         }
 }
